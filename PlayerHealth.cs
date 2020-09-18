80888f6c76f066576fbc8cb7f055fb04612ab143
ch627 <55117528+ch627@users.noreply.github.com>
2020-09-17 19:21:33 -0700

    Health Script
    These health scripts could be used for any player or enemy you want to take damage and be able to die.

---
+++ PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 100; // an interger storing the health amount
+   // public GameObject player;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+        void Die()
+        {
+            Destroy(this.gameObject); // destroy the player game object
+
+
+        }
+    }
+}
+    
