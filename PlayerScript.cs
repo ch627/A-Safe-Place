d148d60ba8062f0322c0a676f8a4650eb953ec8e
ch627 <55117528+ch627@users.noreply.github.com>
2020-09-17 19:52:28 -0700

    Update PlayerScript.cs

--- PlayerScript.cs
+++ PlayerScript.cs
@@ -4,18 +4,14 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    public float MoveSpeed = 5f;
-    public  Rigidbody2D rb2d;
-    public BoxCollider2D bc2d;
-    public float FlipScale;
+    public float MoveSpeed = 5f; // determines the speed the player will moves at, by default, this is 5.
+    public  Rigidbody2D rb2d; // stores the Rigidbody 2D
+    public BoxCollider2D bc2d; // stores the BoxCollider 2D
     // Start is called before the first frame update
     void Start()
     {
-        FlipScale = transform.localScale.x * -1; // assuming this is facing left
         rb2d = GetComponent<Rigidbody2D>(); // get the player rigidbody
         bc2d = GetComponent<BoxCollider2D>(); // get the pllayer collider
-        transform.localScale = new Vector2(FlipScale, transform.localScale.y);
-        transform.localScale = new Vector2(-FlipScale, transform.localScale.y);
     }
 
     // Update is called once per frame
