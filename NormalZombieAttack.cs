2b68064de66223205336bbca8bd17e90653a3311
ch627 <55117528+ch627@users.noreply.github.com>
2020-09-17 19:36:59 -0700

    NormaalZobieAttack Script

---
+++ NormalZombieAttack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalZombieAttack : MonoBehaviour
+{
+    public int normalZombieDamage = 20;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerHealth player = collision.GetComponent<PlayerHealth>(); // try to find a player health script on the object the zombie with
+        player.TakeDamage(normalZombieDamage);
+
+
+            GatesHealth gates = collision.GetComponent<GatesHealth>();
+            gates.TakeDamage(normalZombieDamage);
+    }
+}
