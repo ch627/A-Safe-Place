014169be86f9c3479b8c151718795244ace26761
ch627 <55117528+ch627@users.noreply.github.com>
2020-09-19 12:04:36 -0700

    EnemyMnager Script
    This script is in charge of spawning enemies

---
+++ EnemyManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Transform spawnPoint; // stores a refrence to the spawn point
+    public GameObject enemyPrefab; // stores a refrence to the enemy prefab
+    public bool stopSpawning = false;
+    public float spawnTime = 1.0f;
+    public float spawnDelay = 3.0f;
+
+    private void Start()
+    {
+        StartCoroutine("SpawnDelay");
+        //InvokeRepeating("SpawnNewEnemy", 1.0f, 3.0f);
+
+    }
+
+        void Spawn()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    private void Update()
+    {
+        
+    }
+    public IEnumerator SpawnDelay()
+    {
+        Spawn();
+        yield return new WaitForSeconds(spawnDelay);
+        InvokeRepeating("Spawn", spawnTime, spawnDelay);
+        
+
+    }
+
+}
